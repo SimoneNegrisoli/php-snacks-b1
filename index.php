6131c5e366b624aceecf302a922dc595d9e910d6
SimoneNegrisoli <simo.negrisoli@gmail.com>
2023-11-28 15:32:24 +0100

    scaffolding and readme

---
+++ index.php
@@ -0,0 +1,69 @@
+<!-- ## Snack 1
+
+Creiamo un array contenente le partite di basket di un’ipotetica tappa del calendario. Ogni partita avrà una squadra di casa e una squadra ospite, punti fatti dalla squadra di casa e punti fatti dalla squadra ospite. Stampiamo a schermo tutte le partite con questo schema:
+Olimpia Milano - Cantù | 55-60
+
+## Snack 2
+
+Con un form passare come parametri GET name, mail e age e verificare (cercando i metodi che non conosciamo nella documentazione) che name sia più lungo di 3 caratteri, che mail contenga un punto e una chiocciola e che age sia un numero. Se tutto è ok stampare “Accesso riuscito”, altrimenti “Accesso negato”
+
+## Bonus
+
+Prendere un paragrafo abbastanza lungo, contenente diverse frasi. Prendere il paragrafo e suddividerlo in tanti paragrafi <p>. Ogni punto un nuovo paragrafo. -->
+
+<?php
+$basketMatches = [
+    [
+        'homeTeam' => 'CHICAGO BULLS',
+        'homePoints' => '120',
+        'visitingTeam' => 'BOSTON CELTICS',
+        'visitingPoints' => '30',
+    ],
+    [
+        'homeTeam' => 'CHICAGO BULLS',
+        'homePoints' => '120',
+        'visitingTeam' => 'BOSTON CELTICS',
+        'visitingPoints' => '30',
+    ],
+    [
+        'homeTeam' => 'CHICAGO BULLS',
+        'homePoints' => '120',
+        'visitingTeam' => 'BOSTON CELTICS',
+        'visitingPoints' => '30',
+    ],
+    [
+        'homeTeam' => 'CHICAGO BULLS',
+        'homePoints' => '120',
+        'visitingTeam' => 'BOSTON CELTICS',
+        'visitingPoints' => '30',
+    ],
+    [
+        'homeTeam' => 'CHICAGO BULLS',
+        'homePoints' => '120',
+        'visitingTeam' => 'BOSTON CELTICS',
+        'visitingPoints' => '30',
+    ]
+]
+
+    ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Snacks</title>
+</head>
+
+<body>
+
+    <!-- SNACK1 -->
+    <?php
+
+    ?>
+
+
+</body>
+
+</html>
---
+++ readme.md
@@ -0,0 +1,14 @@
+# PHP Snacks
+
+## Snacks 1
+
+Let's create an array containing the basketball games of a hypothetical stage of the calendar. Each match will have a home team and a guest team, points scored by the home team and points scored by the guest team. We print all the games on the screen with this scheme:
+Olimpia Milan - Cantù | 55-60
+
+## Snacks 2
+
+With a form, pass name, mail and age as GET parameters and verify (looking for methods that we don't know in the documentation) that name is longer than 3 characters, that mail contains a dot and an at sign and that age is a number. If everything is ok print “Access successful”, otherwise “Access denied”
+
+## Bonuses
+
+Take a fairly long paragraph, containing several sentences. Take the paragraph and divide it into many paragraphs <p>. Each point a new paragraph.
